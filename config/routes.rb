eb33c59633c5ccc508eaf675de41cb065edf577b
anton-sales <anton.arus@gmail.com>
2018-07-31 13:00:55 +1200

    added hello world users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :micro_posts
+  resources :users
+  root 'users#index'
+  # root 'micro_posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
